0438aaa20fdb39e3704fc384e6debbd8431ab819
user <nana-aslan@hotmail.com>
2017-10-11 10:58:13 +0300

    aa

--- app/src/main/java/com/aslan/amani/shoppinglist/ItimActivity.java
+++ app/src/main/java/com/aslan/amani/shoppinglist/ItimActivity.java
@@ -2,19 +2,42 @@ package com.aslan.amani.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
-
-public class ItimActivity extends AppCompatActivity {
+//                                                 1.
+public class ItimActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView ItemName;
     private TextView etAmount;
     private TextView etUnits;
-
+    private ImageButton iBtnImage;
+    private Button BtnSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itim);
+        ItemName=(TextView) findViewById (R.id.ItemName);
+        etAmount=(TextView)findViewById(R.id.etAmount);
+        etAmount=(TextView)findViewById(R.id.etUnits);
+        iBtnImage=(ImageButton)findViewById(R.id.iBtnImage);
+        BtnSave=(Button)findViewById(R.id.BtnSave);
+
+//4.
+        BtnSave.setOnClickListener(this);
 
 
     }
 
+    //2. alt enter
+    @Override
+    public void onClick(View view) {
+        //5
+        if (view==BtnSave)
+        {
+            // reaction
+        }
+
+
+    }
 }
--- app/src/main/java/com/aslan/amani/shoppinglist/SignUpActivity.java
+++ app/src/main/java/com/aslan/amani/shoppinglist/SignUpActivity.java
@@ -2,15 +2,26 @@ package com.aslan.amani.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
     private TextView etName;
     private TextView etEmail;
+    private TextView password;
+    private Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        etName = (TextView) findViewById(R.id.etName);
+        etEmail = (TextView) findViewById(R.id.etEmail);
+        password = (TextView) findViewById(R.id.password);
+        save = (Button) findViewById (R.id.Save);
+
+
+
     }
+
 }
